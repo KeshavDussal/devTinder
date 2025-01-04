3fe3f73f2b7c82214ff25c0a98285f7af316a7ac
Keshav Dussal <ksdussal96@gmail.com>
2025-01-04 16:15:01 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,29 +1,11 @@
 const express = require("express");
 
 const app = express();
-//function inside use is known as request handler
-app.use("/user", (req, res) => {
-  res.send("HAHAHAHAHH");
-});
 
 //But below will only handle get API calls.
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Akshay", lastName: "Saini" });
-});
-
-app.post("/user", (req, res) => {
-  //Saving data to DB
-  res.send("Data Successfully saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-  //Saving data to DB
-  res.send("Deleted Successfully");
-});
-
-//if you "use" This will match all the http method API calls to the /test.
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
 });
 
 app.listen(7777, () => {
