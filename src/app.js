9a49ab32925b15209c2b0732753c8d23145dafd5
Keshav Dussal <ksdussal96@gmail.com>
2025-02-01 21:08:24 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Akshay", lastName: "Saini" });
-});
+//But below will only handle get API calls.
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user");
+    next(); //Next goes to next response handler and you will see 2nd Response as output.
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2");
+    //res.send("2nd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 3");
+    //res.send("3rd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4");
+    //res.send("4th Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4");
+    res.send("5th Response!!");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is succesfully listening on port 7777...");
