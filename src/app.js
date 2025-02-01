9bfb884aa5a9fe324887890155d48bb78357539b
Keshav Dussal <ksdussal96@gmail.com>
2025-02-01 22:38:39 +0530

    Without middleware authorization logic repeated

--- src/app.js
+++ src/app.js
@@ -2,34 +2,33 @@ const express = require("express");
 
 const app = express();
 
-//But below will only handle get API calls.
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling the route user");
-    next(); //Next goes to next response handler and you will see 2nd Response as output.
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 2");
-    //res.send("2nd Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 3");
-    //res.send("3rd Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 4");
-    //res.send("4th Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 4");
-    res.send("5th Response!!");
+app.get("/admin/getAllData", (req, res) => {
+  //Logic of checking authentication/authorized
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (isAdminAuthorized) {
+    //res.send - default status is 200
+    res.send("All data sent")
   }
-);
+  else {
+    //res.status - changing the default status to mentioned status
+    res.status(401).send("UnAuthorized Request");
+  }
+})
 
+app.get("/admin/deleteUser", (req, res) => {
+  //Logic of checking authentication/authorized
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (isAdminAuthorized) {
+    //res.send - default status is 200
+    res.send("Deleted a user")
+  }
+  else {
+    //res.status - changing the default status to mentioned status
+    res.status(401).send("UnAuthorized Request");
+  }
+})
 app.listen(7777, () => {
   console.log("Server is succesfully listening on port 7777...");
 });
