4c375bf9f9237ce6bf0c0612341f5719b97992f0
Keshav Dussal <ksdussal96@gmail.com>
2025-02-13 22:10:16 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ const express = require("express");
 const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user")
+//Middleware activated for all routes - converting json to js object 
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
+
   //Creating a new instance of the User Model
-  const user = new User({
-    firstName: "Sachin",
-    lastName: "Tendulkar",
-    emailId: "sachin@tendulkar.com",
-    password: "sachin@123"
-  });
+  const user = new User(req.body);
   //Handle DB operation always in try catch block
   try {
     //Returns you a promise.
